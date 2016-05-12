45ab5fa46f1ff512d69923b16584fa6c1c3fb050
KerlyNaranjo <k.erly5@hotmail.com>
2016-05-12 08:51:49 -0500

    Cambios
    Aun no funciona

--- Ejercicio4.1.py
+++ Ejercicio4.1.py
@@ -1,4 +1,21 @@
 #Ingresar una frase y contar el número de palabras
 
-frase = str(input('Ingrese una frase: '))
-print (len(frase.split(" ")))
+#frase = str(input('Ingrese una frase: '))
+
+def contar():
+	return len(frase.split(' '))
+
+def creartxt():
+	archivo = open('Frases.txt','w')
+	archivo.close()
+
+def escribirtxt():
+	archivo = open('Frases.txt','w')
+	#frase = archivo.write((input('Ingrese las frases que desee: '))
+	frase = archivo.write(str(input('Ingrese una frase: ')))
+	#archivo.write(len(frase.split(' ')))
+	archivo.close()
+	print (len(frase.split(' ')))
+
+creartxt()
+escribirtxt()
