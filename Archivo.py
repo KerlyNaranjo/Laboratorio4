e995376a7ad37891e9725d0517da61f64c557ccb
katherinelasluisa <katherinelasluisa7@gmail.com>
2016-05-12 09:07:28 -0500

    cambios3
    funcion de leer

--- Archivo.py
+++ Archivo.py
@@ -1,23 +1,9 @@
 #crear un archivo de texto 
 
-def creartxt():
-	archi=open('texto.txt', 'w')
-	archi.close()
-
-#def contador():
-	#return  (len(frase.split(" "))
-
-def escribirtxt():
-	archi=open('texto.txt','w')
-	frase=archi.write (str(input("ingresar una frase:")))
-	#archi.write('HOLA MUNDO COMO ESTAS!!!\n')
-	archi.write(len(frase.split(" ")))
-	archi.close()
-
-def grabartxt():
-	archi=open('texto.txt','a')
-	archi.close()
-
-creartxt()
-escribirtxt()
-grabartxt()
+def leertxt():
+	archi=open('texto.txt','r')
+	linea=archi.readline()
+	while linea!="":
+		print linea
+		linea=archi.readline()
+archi.close()
